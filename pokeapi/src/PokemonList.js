a7b2b3eb98c0a082b66f1edac4562685ef7b0aac
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2024-03-20 21:27:24 -0700

    InformacionYBoton

--- pokeapi/src/PokemonList.js
+++ pokeapi/src/PokemonList.js
@@ -5,6 +5,8 @@ import './PokemonList.css';
 function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [expandedPokemon, setExpandedPokemon] = useState(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -59,6 +61,41 @@ function PokemonList() {
     return types.length > 0 && types[0].type.name in typeColors ? typeColors[types[0].type.name] : defaultColor;
   };
 
+  const handleToggleDetails = (pokemon) => {
+    if (expandedPokemon === pokemon) {
+      setExpandedPokemon(null); // Cerrar detalles si ya están expandidos
+      setIsSidebarOpen(false); // Ocultar el sidebar al cerrar los detalles
+    } else {
+      setExpandedPokemon(pokemon); // Mostrar detalles si no están expandidos
+      setIsSidebarOpen(true); // Mostrar el sidebar al abrir los detalles
+    }
+  };
+
+  const handleCloseDetails = () => {
+    setExpandedPokemon(null);
+    setIsSidebarOpen(false);
+  };
+
+  const PokemonDetailsSidebar = ({ pokemon, onClose }) => {
+    if (!pokemon) return null; // No mostrar nada si no hay Pokemon seleccionado
+  
+    return (
+      <div className="pokemon-details-overlay">
+        <div className="pokemon-details-sidebar-wrapper">
+          <button className="close-button" onClick={onClose}>X</button>
+          <h2>Detalles de {pokemon.name}</h2>
+          <div className="pokemon-details-content">
+            <p>HP: {pokemon.stats[0].base_stat}</p>
+            <p>Ataque: {pokemon.stats[1].base_stat}</p>
+            <p>Defensa: {pokemon.stats[2].base_stat}</p>
+            <p>Ataque Especial: {pokemon.stats[3].base_stat}</p>
+            <p>Velocidad: {pokemon.stats[5].base_stat}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="pokemon-container">
       <SearchBar placeholder="Buscar pokemón" onSearch={handleSearch} />
@@ -67,9 +104,13 @@ function PokemonList() {
           <div key={pokemon.id} className="pokemon-card" style={{ backgroundColor: getBackgroundColor(pokemon.types) }}>
             <h3>{pokemon.name}</h3>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <button className="ver-mas-button" onClick={() => handleToggleDetails(pokemon)}>
+              {expandedPokemon === pokemon ? 'Cerrar Detalles' : 'Ver más'}
+            </button>
           </div>
         ))}
       </div>
+      {isSidebarOpen && <PokemonDetailsSidebar pokemon={expandedPokemon} onClose={() => setIsSidebarOpen(false)} />}
     </div>
   );
 }
