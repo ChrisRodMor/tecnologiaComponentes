afc6b9a636b147b3bc39cd720ed06b92f14fb478
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2024-04-22 17:49:22 -0700

    getPatchPostYDeleteFinalizados

--- eg-api/routes/users.js
+++ eg-api/routes/users.js
@@ -20,12 +20,28 @@ router.get('/', function(req, res, next) {
   res.json(users);
 });
 
+/*  POST create user */
+router.post('/', function(req, res, next) {
+
+  //crear un nuevo usuario con los valores enviados por el body en la peticion
+  var newUser = req.body;
+
+  /* Si no se envian los campos requeridos (nombre y correo), retornamos error 400 Bad Request*/
+  if (!newUser.hasOwnProperty('name') || !newUser.hasOwnProperty('email')){
+    return res.status(400).send({error: 'Missing properties'}).end();
+  }
+
+  users.push(newUser);
+  res.json(newUser);
+});
+
 /* PATCH users listing. */
 router.patch('/:id', function (req,res){
   var user = req.body;
   if(!user || !user.name){
     return res.status(400).send({error:'Missing parameter'})
       }
+
       //buscamos el usuario por su ID en la BD
       let index= users.findIndex((item)=>{
         return item.id=== parseInt(req.params.id)
@@ -44,9 +60,27 @@ router.patch('/:id', function (req,res){
       }
 });
 
-/* PATCH users listing. */
-
 /* DELETE users listing. */
+router.delete('/:id', function(req, res, next) {
+  var id = parseInt(req.params.id);
 
+  //buscamos el usuario por su ID en la BD
+  let userPosition = users.findIndex((item)=>{
+    return item.id=== parseInt(req.params.id)
+  });
+
+  //Si no lo encuentra
+  if(userPosition < 0){
+    return res.status(400).
+    send({ error : 'User not found'})
+  }else{
+    
+    //PARA BORRARLO
+    users.splice(userPosition,1);
+
+    //devolvemos la actualización
+    res.json(users)
+  }
+});
 
 module.exports = router;
